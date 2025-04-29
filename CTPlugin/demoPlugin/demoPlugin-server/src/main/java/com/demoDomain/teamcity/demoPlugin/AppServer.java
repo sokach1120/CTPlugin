8c539c21bae6989b507e844748b922da3fa35576
a181 <a181@noreply.gitcode.com>
2025-04-29 16:16:10 +0800

    ss

--- CTPlugin/demoPlugin/demoPlugin-server/src/main/java/com/demoDomain/teamcity/demoPlugin/AppServer.java
+++ CTPlugin/demoPlugin/demoPlugin-server/src/main/java/com/demoDomain/teamcity/demoPlugin/AppServer.java
@@ -9,8 +9,14 @@ import org.springframework.web.servlet.ModelAndView;
 import jetbrains.buildServer.controllers.BaseController;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.WebControllerManager;
+import jetbrains.buildServer.vcs.BuildPatchPolicy;
+import jetbrains.buildServer.vcs.CollectChangesPolicy;
+import jetbrains.buildServer.vcs.VcsExtension;
+import jetbrains.buildServer.vcs.VcsFileContentProvider;
+import jetbrains.buildServer.vcs.VcsSupportContext;
+import jetbrains.buildServer.vcs.VcsSupportCore;
 
-public class AppServer extends BaseController {
+public class AppServer extends BaseController implements VcsSupportContext {
     private PluginDescriptor myDescriptor;
 
     public AppServer(WebControllerManager manager, PluginDescriptor descriptor) {
@@ -22,5 +28,35 @@ public class AppServer extends BaseController {
     @Override
     protected ModelAndView doHandle(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
         return new ModelAndView(myDescriptor.getPluginResourcesPath("example.jsp"));
+    }
+
+    @Override
+    public <T extends VcsExtension> T getVcsExtension(Class<T> extensionClass) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVcsExtension'");
+    }
+
+    @Override
+    public VcsSupportCore getCore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCore'");
+    }
+
+    @Override
+    public VcsFileContentProvider getContentProvider() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getContentProvider'");
+    }
+
+    @Override
+    public CollectChangesPolicy getCollectChangesPolicy() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCollectChangesPolicy'");
+    }
+
+    @Override
+    public BuildPatchPolicy getBuildPatchPolicy() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBuildPatchPolicy'");
     }
 }
